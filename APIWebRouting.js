a50b560550ca2c17ad3e82ef38672c1cbce4e748
Tiana Zou <tianazou@Tianas-MacBook-Pro.local>
2024-03-12 22:04:16 +1100

    adding file

---
+++ APIWebRouting.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const app = express();
+
+//creating a basic handler
+class Product {
+
+    constructor(id, name, price, description)  {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+
+    }
+}
+
+//storing some default products to store into the array
+const products = [
+    new Product (1, "Strawberry", 5.00, "Strawberry description"),
+    new Product (2, "Chocolate", 5.00, "Chocolate description"),
+    new Product (3, "Banana", 5.00, "Banana description"),
+
+
+];
+
+//console.log(products[2]);
+
+for(let i = 0; i < products.length; i++) {
+    console.log(products[i].name);
+  }
